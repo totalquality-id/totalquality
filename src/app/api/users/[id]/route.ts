9e23536f7c870cf7ffba5bcfd6753c37eee13b86
marcaroon <ammarqorni@gmail.com>
2025-11-06 14:05:40 +0700

    `Update route parameters to use Promise<{ id: string }> instead of { params: { id: string } }`

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -2,24 +2,27 @@ import { getUser, updateUser, removeUser } from "@/controllers/userController";
 
 export async function GET(
   req: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const id = parseInt(params.id);
+  const { id: idString } = await params;
+  const id = parseInt(idString);
   return getUser(id);
 }
 
 export async function PATCH(
   req: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const id = parseInt(params.id);
+  const { id: idString } = await params;
+  const id = parseInt(idString);
   return updateUser(id, req);
 }
 
 export async function DELETE(
   req: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const id = parseInt(params.id);
+  const { id: idString } = await params;
+  const id = parseInt(idString);
   return removeUser(id);
 }
